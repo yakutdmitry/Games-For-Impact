ad720215b7f3bdcf1da4b5613adf0d31d34c09df
DmitryY <D23125246@mytudublin.ie>
2026-03-26 13:13:28 +0000

    Should be fully ready for the playtest

--- Assets/Scripts/RepairCustomerDest.cs
+++ Assets/Scripts/RepairCustomerDest.cs
@@ -4,12 +4,13 @@ using UnityEngine.AI;
 
 public class RepairCustomerDest : MonoBehaviour
 {
-    public GameObject popUP;
+    [SerializeField] private GameObject popUP;
     public Transform GarmentSpawner;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("RepairCustomer"))
         {
+            popUP = GameObject.Find("PopupManager").transform.Find("Popup1").gameObject;
             other.GetComponent<NavMeshAgent>().isStopped = true;
             popUP.SetActive(true);
             Instantiate(other.GetComponent<RepairCustomer>().pickedClothing, GarmentSpawner);
